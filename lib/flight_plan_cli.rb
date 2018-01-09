63c67fdb89cf198856b5ab8192adcc7c160bb9f9
John Cleary <john@createk.io>
2018-01-09 07:38:49 +0000

    add skeleton for `fp ls` (listing issues)

---
+++ lib/flight_plan_cli.rb
@@ -0,0 +1,2 @@
+require 'flight_plan_cli/initializer'
+require 'flight_plan_cli/version'
---
+++ lib/flight_plan_cli/initializer.rb
@@ -0,0 +1,10 @@
+require 'thor'
+
+module FlightPlanCli
+  class Initializer < Thor
+    desc 'ls', 'List open issues'
+    def ls
+      puts 'coming soon'
+    end
+  end
+end
