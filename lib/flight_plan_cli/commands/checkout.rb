4f8ef21264ceae153f95f094a02001a3f6bf5b50
John Cleary <john@createk.io>
2018-01-31 22:23:13 +0000

    create local branch if none exists

--- lib/flight_plan_cli/commands/checkout.rb
+++ lib/flight_plan_cli/commands/checkout.rb
@@ -45,9 +45,25 @@ module FlightPlanCli
 
       def new_branch_for(issue)
         read_config
-        branches = client.board_tickets(
-          board_id: board_id, repo_id: repo_id, remote_number: issue
-        )
+        branches = client.board_tickets(remote_number: issue)
+        # TODO: update flight_plan to only return one issue when remote_numer is provided
+        branches = branches.select { |b| b['ticket']['remote_number'] == issue }
+        return false unless branches.count == 1
+
+        branch_name = branch_name(branches.first)
+
+        puts "Creating new branch #{branch_name} from master".green
+
+        git.branches.create(branch_name, 'origin/master')
+        git.checkout(branch_name)
+      end
+
+      def branch_name(branch)
+        "feature/##{branch['ticket']['remote_number']}-" +
+          branch['ticket']['remote_title']
+          .gsub(/[^a-z0-9\s]/i, '')
+          .tr(' ', '-')
+          .downcase
       end
 
       def local_branches
