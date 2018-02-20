9d2978b9ca2a1e55894ac1866860930f4f75a44d
John Cleary <john@createk.io>
2018-02-20 21:35:58 +0100

    add test for creating a new branch if one does not exist on locally or on the remote.

--- spec/lib/commands/checkout_spec.rb
+++ spec/lib/commands/checkout_spec.rb
@@ -3,26 +3,35 @@ require 'spec_helper'
 RSpec.describe FlightPlanCli::Commands::Checkout do
   describe '#process' do
     subject { described_class.new(issue_no) }
-    let(:issue_no) { '1234' }
-    let(:issue_json) { { 'issue_no' => issue_no } }
-    let(:git) { double('git', branches: branches) }
+    let(:issue_no) { '5467' }
+    let(:issue_title) { 'My New Feature' }
+    let(:title_dash_case) { 'my-new-feature' }
+    let(:issue_json) { { 'issue_no': issue_no } }
+    let(:git) { double('git', branches: branches, remote: remote) }
     let(:branches) { double('branches', local: local_branches, remote: remote_branches) }
     let(:branch) { double('branch', name: branch_name) }
-    let(:branch_name) { 'issue/#1234-title' }
+    let(:branch_name) { "feature/##{issue_no}-#{title_dash_case}" }
     let(:remote) { double('remote', fetch: true) }
     let(:local_branches) { [] }
     let(:remote_branches) { [] }
+    let(:ticket) {
+      {
+        'ticket' => {
+          'remote_number' => issue_no,
+          'remote_title' => issue_title
+        }
+      }
+    }
+
+    before do
+      allow(subject).to receive(:git) { git }
+    end
 
     context 'when there is a local branch for the issue' do
       let(:local_branches) { [branch] }
       let(:output_text) { /Checking out local branch/ }
 
       it 'switches to the local branch' do
-        stub_request(:get, 'http://dev.createk.io/api/board_tickets.json')
-          .with(query: hash_including(remote_number: issue_no))
-          .to_return(body: [issue_json].to_s)
-        allow(subject).to receive(:git) { git }
-
         expect(git).to receive(:checkout).with(branch_name)
         expect { subject.process }.to output(output_text).to_stdout
       end
@@ -30,16 +39,29 @@ RSpec.describe FlightPlanCli::Commands::Checkout do
 
     context 'when there is a remote branch for the issue' do
       let(:remote_branches) { [branch] }
-      let(:output_text) { %r{Checking out and tracking remote branch 'issue\/#1234-title} }
+      let(:output_text) { /Checking out and tracking remote branch '#{branch_name}'/ }
 
       it 'checks out the remote branch locally' do
-        stub_request(:get, 'http://dev.createk.io/api/board_tickets.json')
-          .with(query: hash_including(remote_number: issue_no))
-          .to_return(body: [issue_json].to_s)
-        allow(subject).to receive(:git) { git }
-        expect(git).to receive(:remote) { remote }
-
         expect(git).to receive(:checkout).with(branch_name)
+        expect { subject.process }.to output(output_text).to_stdout
+      end
+    end
+
+    context 'when there is no local or remote branch for an issue' do
+      let(:output_text) { /Creating new branch #{branch_name} from master/ }
+      let(:flight_plan) { double('FlightPlan') }
+
+      it 'creates a local branch' do
+        expect(subject).to receive(:flight_plan) { flight_plan }
+        expect(flight_plan).to receive(:board_tickets)
+          .with(remote_number: issue_no).and_return([ticket])
+        expect(git).to receive(:checkout).with('master')
+        expect(git).to receive(:pull)
+        expect(git).to receive(:branch)
+          .with(branch_name)
+          .and_return(branch)
+        expect(branch).to receive(:checkout)
+
         expect { subject.process }.to output(output_text).to_stdout
       end
     end
