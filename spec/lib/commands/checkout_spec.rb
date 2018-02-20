35c09407ee36f87c5492e7cc0f208e25bfa74775
John Cleary <john@createk.io>
2018-02-20 08:06:35 +0100

    add a test for local checkout

---
+++ spec/lib/commands/checkout_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.describe FlightPlanCli::Commands::Checkout do
+  describe '#process' do
+    subject { described_class.new(issue_no) }
+    let(:issue_no) { '1234' }
+    let(:issue_json) { { 'issue_no' => issue_no } }
+    let(:git) { double('git', branches: branches) }
+    let(:branches) { double('branches', local: local_branches) }
+    let(:branch) { double('branch', name: branch_name) }
+    let(:branch_name) { 'issue/#1234-title' }
+
+    context 'when there is a local branch for the issue' do
+      let(:local_branches) { [branch] }
+      let(:output_text) { /Fetching|Checking out local branch/ }
+
+      it 'switches to the local branch' do
+        stub_request(:get, 'http://dev.createk.io/api/board_tickets.json')
+          .with(query: hash_including(remote_number: issue_no))
+          .to_return(body: [issue_json].to_s)
+        allow(subject).to receive(:git) { git }
+
+        expect(git).to receive(:checkout).with(branch_name)
+        expect { subject.process }.to output(output_text).to_stdout
+      end
+    end
+  end
+end
