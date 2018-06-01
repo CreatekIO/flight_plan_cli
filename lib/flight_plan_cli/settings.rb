45d61b9c1e738e0cb5c0a9c38b039fb9c5f22ec2
Rob Paskin <rjpaskin+git@gmail.com>
2018-06-01 14:27:26 +0100

    Refactor Settings to use singleton pattern
    Makes it more amenable to testing.

--- lib/flight_plan_cli/settings.rb
+++ lib/flight_plan_cli/settings.rb
@@ -1,33 +1,37 @@
+require 'singleton'
+
 module FlightPlanCli
-  module Settings
+  class SettingsRecord
+    include Singleton
+
     CONFIG_YAML_PATH = '.flight_plan_cli/config.yml'.freeze
     USER_YAML_PATH = '.flight_plan_cli/user.yml'.freeze
 
-    def self.board_id
+    def board_id
       config['board_id']
     end
 
-    def self.repo_id
+    def repo_id
       config['repo_id']
     end
 
-    def self.default_swimlane_ids
+    def default_swimlane_ids
       config['ls']['default_swimlane_ids']
     end
 
-    def self.api_url
+    def api_url
       config['api_url']
     end
 
-    def self.api_key
+    def api_key
       config['flight_plan_api_key']
     end
 
-    def self.api_secret
+    def api_secret
       config['flight_plan_api_secret']
     end
 
-    def self.config
+    def config
       @config ||=
         begin
           check_config_exists
@@ -37,10 +41,12 @@ module FlightPlanCli
         end
     end
 
-    def self.check_config_exists
+    def check_config_exists
       return if FileTest.exist?(CONFIG_YAML_PATH)
       puts "#{CONFIG_YAML_PATH} not found"
       exit 1
     end
   end
+
+  Settings = SettingsRecord.instance
 end
